8a59ac4c5b381b83c06fc16578e1a3acc75ba9b4
fhellipevalentin <fhellipereis@outlook.com>
2024-01-15 17:37:33 -0300

    login em memoria concluido na view

--- src/main/java/com/example/demo/configuration/SecurityConfig.java
+++ src/main/java/com/example/demo/configuration/SecurityConfig.java
@@ -5,8 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 public class SecurityConfig {
@@ -16,26 +19,40 @@ public class SecurityConfig {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests()
                 .requestMatchers("/", "/home").permitAll()
-                .and().formLogin(login -> login
-                .loginPage("/login")
-                .failureUrl("/login-error.html"))
-                .logout(logout -> logout
-                        .logoutSuccessUrl("/index.html"));
+                .anyRequest().authenticated()
+                .and()
+                .formLogin(login -> login
+                        .loginPage("/login")
+                        .successHandler(loginSuccessHandler())
+                        .permitAll())
+                .logout(logout -> logout.permitAll());
         return http.build();
 	}
 	
 	@Bean
     InMemoryUserDetailsManager userDetailsService() {
         UserDetails admin = User.withUsername("admin")
-                .password("12345")
+                .password(passwordEncoder().encode("12345"))
+                .roles("ADMIN")
                 .authorities("admin")
                 .build();
         UserDetails user = User.withUsername("user")
                 .username("user")
-                .password("12345")
+                .roles("USER")
+                .password(passwordEncoder().encode("12345"))
                 .authorities("read")
                 .build();
         return new InMemoryUserDetailsManager(admin, user);
 
 	}
+
+    @Bean
+    AuthenticationSuccessHandler loginSuccessHandler() {
+        return (request, response, authentication) -> response.sendRedirect("/index");
+    }
+    
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
